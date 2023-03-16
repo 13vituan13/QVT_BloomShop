84c33b61b2c99d7e738450b7630a6474b545118f
ViTuan <tuanvq@hp-backup.com>
2023-03-16 16:22:46 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController AS User;
-use App\Http\Controllers\UserLoginController AS UserLogin;
-use App\Http\Controllers\UserSignUpController AS UserSignUp;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserLoginController;
+use App\Http\Controllers\UserSignUpController;
 
 
-use App\Http\Controllers\AdminController AS Admin;
-use App\Http\Controllers\AdminLoginController AS AdminLogin;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,35 +22,46 @@ use App\Http\Controllers\AdminLoginController AS AdminLogin;
 */
 
 //______________________________ USER SITE_______________________________
-Route::get('/', [User::class, 'home'])->name('home');
-Route::get('/about', [User::class, 'about'])->name('about');
-Route::get('/contact', [User::class, 'contact'])->name('contact');
-Route::get('/product', [User::class, 'product'])->name('product');
-Route::get('/services', [User::class, 'services'])->name('services');
-Route::get('/product_detail', [User::class, 'product_detail'])->name('product_detail');
-Route::get('/cart', [User::class, 'cart'])->name('cart');
+Route::get('/', [UserController::class, 'home'])->name('home');
+Route::get('/about', [UserController::class, 'about'])->name('about');
+Route::get('/contact', [UserController::class, 'contact'])->name('contact');
+Route::get('/product', [UserController::class, 'product'])->name('product');
+Route::get('/services', [UserController::class, 'services'])->name('services');
+Route::get('/product_detail', [UserController::class, 'product_detail'])->name('product_detail');
+Route::get('/cart', [UserController::class, 'cart'])->name('cart');
 
-// ****** LOGIN -- SIGNUP ******
-Route::get('/login', [UserLogin::class, 'login'])->name('login');
-Route::get('/logout', [UserLogin::class, 'logout'])->name('logout');
-Route::post('login_submit', [UserLogin::class, 'post_Login'])->name('login.submit');
-Route::get('/sign_up', [UserSignUp::class, 'sign_up'])->name('sign_up');
-Route::post('/sign_up_submit', [UserSignUp::class, 'sign_up_submit'])->name('sign_up.submit');
-Route::post('/check_email_exist', [UserSignUp::class, 'check_email_exist'])->name('check.email');
+// ****** LOGIN ******
+Route::get('/login', [UserLoginController::class, 'login'])->name('login');
+Route::post('login_submit', [UserLoginController::class, 'post_Login'])->name('login.submit');
+Route::get('/logout', [UserLoginController::class, 'logout'])->name('logout');
+
+// ****** SIGNUP ******
+Route::get('/sign_up', [UserSignUpController::class, 'sign_up'])->name('sign_up');
+Route::post('/sign_up_submit', [UserSignUpController::class, 'sign_up_submit'])->name('sign_up.submit');
+Route::post('/check_email_exist', [UserSignUpController::class, 'check_email_exist'])->name('check.email');
+
+/*
+|
+|
+|-------------------------------------------------------------------------
+| 
+| 
+*/
 
 //______________________________ ADMIN SITE_______________________________
-Route::get('/test', [Admin::class, 'test'])->name('test');
+Route::get('/test', [AdminController::class, 'test'])->name('test');
 
 Route::prefix('/admin')->group(function() {
-    Route::get('login', [AdminLogin::class, 'login'])->name('admin.login');
-    Route::get('logout', [AdminLogin::class, 'logout'])->name('admin.logout');
-
-    Route::post('login_submit', [AdminLogin::class, 'post_Login'])->name('admin.login.submit');
+    // ****** LOGIN ******
+    Route::get('login', [AdminController::class, 'login'])->name('admin.login');
+    Route::post('login_submit', [AdminController::class, 'post_Login'])->name('admin.login.submit');
+    Route::get('logout', [AdminController::class, 'logout'])->name('admin.logout');
 });
 
 Route::prefix('/admin')->middleware(['admin.session'])->group(function() {
-    Route::get('product', [Admin::class, 'product'])->name('admin.product');
-    Route::get('product_detail/{id?}', [Admin::class, 'product_detail'])->name('admin.product_detail');
+    // ****** PRODUCT ******
+    Route::get('product', [AdminLoginController::class, 'product'])->name('admin.product');
+    Route::get('product_detail/{id?}', [AdminLoginController::class, 'product_detail'])->name('admin.product_detail');
 });
 
 
