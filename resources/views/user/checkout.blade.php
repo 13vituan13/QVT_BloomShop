3667d57c58f4ce86a6295c5c16dbfe57db061088
Vi Tuan <qvituan13@gmail.com>
2023-03-23 23:28:35 +0700

    update code

--- resources/views/user/checkout.blade.php
+++ resources/views/user/checkout.blade.php
@@ -60,7 +60,7 @@
                 <div class="col-md-5 col-lg-4 order-md-last">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-primary">Giỏ hàng của bạn</span>
-                        <span class="badge bg-primary rounded-pill">3</span>
+                        <span class="badge bg-primary rounded-pill">{{ $cartCounter }}</span>
                     </h4>
                     <ul class="list-group mb-3">
                         @if ($cartCounter > 0)
